68ca6d61f97067660ff83d6a1aed859c92be9cb3
Eldhose Chackochan <38857974+Ldoz13@users.noreply.github.com>
2025-09-14 18:15:12 +0530

    chnage convert.py

--- convert.py
+++ convert.py
@@ -7,9 +7,8 @@ directory = 'book'
 # List all files in the directory ending with ".jpg"
 image_files = [os.path.join(directory, i) for i in os.listdir(directory) if i.endswith(".jpg")]
 
-# Sort files naturally if filenames contain numbers
-image_files.sort(key=lambda x: int(os.path.basename(x).split('.')[0]))  
-
 # Convert image files to PDF
 with open("output.pdf", "wb") as f:  # Open the output file in binary write mode
     f.write(img2pdf.convert(image_files))
+
+# ocrmypdf input.pdf output.pdf     for OCR
